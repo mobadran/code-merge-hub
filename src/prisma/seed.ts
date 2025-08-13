67b0e67c75c613f9979afa81d6c848b7c5e3cc33
mobadran <mohamedbadran1492008@gmail.com>
2025-08-13 07:13:40 +0300

    updated seed.ts to have hashed passwords

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -6,20 +6,35 @@ const userData: Prisma.UserCreateInput[] = [
   {
     name: "Alice",
     email: "alice@prisma.io",
-    password: "password123",
+    password: "$2b$10$8Tw0l.Wq9WjFqfXQ7DHxp.AjadCrShdR7ClLf5KUqNF3SdkymcJNO",
   },
   {
     name: "Bob",
     email: "bob@prisma.io",
-    password: "password123",
+    password: "$2b$10$8Tw0l.Wq9WjFqfXQ7DHxp.AjadCrShdR7ClLf5KUqNF3SdkymcJNO",
   },
 ];
-
+// Password is: "password"
 
 export async function main() {
   for (const u of userData) {
-    await prisma.user.create({ data: u });
+    await prisma.user.upsert({
+      where: { email: u.email }, // Search by unique field
+      update: {
+        name: u.name,
+        password: u.password,
+      },
+      create: u,
+    });
   }
 }
 
-main();
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
